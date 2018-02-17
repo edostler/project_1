0a56cdac08dd2df2f01528b5832dcdd2a7d90b74
Ed Ostler <edward.o@simul8.com>
2018-02-17 17:43:17 +0000

    Adds seed data and calls delete_all and save functions (to be written). Also adds date in for transactions - to be considered as a placeholder to avoid future structural issues

--- db/banked.sql
+++ db/banked.sql
@@ -1,12 +1,6 @@
-DROP TABLE transactions CASCADE;
-DROP TABLE tags CASCADE;
 DROP TABLE merchants CASCADE;
-
-CREATE TABLE tags (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
-  total_spend FLOAT
-);
+DROP TABLE tags CASCADE;
+DROP TABLE transactions CASCADE;
 
 CREATE TABLE merchants (
   id SERIAL PRIMARY KEY,
@@ -14,10 +8,17 @@ CREATE TABLE merchants (
   total_spend FLOAT
 );
 
-CREATE TABLE transactions (
+CREATE TABLE tags (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
+  total_spend FLOAT
+);
+
+CREATE TABLE transactions (
+  id SERIAL PRIMARY KEY,
+  description VARCHAR(255) NOT NULL,
   value FLOAT NOT NULL,
-  tag_id INT REFERENCES tags(id) ON DELETE CASCADE,
-  merchant_id INT REFERENCES merchants(id) ON DELETE CASCADE
+  spend_date DATE NOT NULL,
+  merchant_id INT REFERENCES merchants(id) ON DELETE CASCADE,
+  tag_id INT REFERENCES tags(id) ON DELETE CASCADE
 );
---
+++ db/seeds.rb
@@ -0,0 +1,86 @@
+require_relative( "../models/merchant.rb" )
+require_relative( "../models/tag.rb" )
+require_relative( "../models/transaction.rb" )
+require("pry-byebug")
+
+Merchant.delete_all()
+Tag.delete_all()
+Transaction.delete_all()
+
+merchant1 = Merchant.new({
+  "name" => "Sainsburys",
+  "total_spend" => 0
+})
+merchant1.save()
+merchant2 = Merchant.new({
+  "name" => "Dominos",
+  "total_spend" => 0
+})
+merchant2.save()
+merchant3 = Merchant.new({
+  "name" => "Bier Halle",
+  "total_spend" => 0
+})
+merchant3.save()
+merchant4 = Merchant.new({
+  "name" => "Slouch",
+  "total_spend" => 0
+})
+merchant4.save()
+
+tag1 = Tag.new({
+  "name" => "Food Shop",
+  "total_spend" => 0
+})
+tag1.save()
+tag2 = Tag.new({
+  "name" => "Take-away",
+  "total_spend" => 0
+})
+tag2.save()
+tag3 = Tag.new({
+  "name" => "Restaurant",
+  "total_spend" => 0
+})
+tag3.save()
+tag4 = Tag.new({
+  "name" => "Bar",
+  "total_spend" => 0
+})
+tag4.save()
+
+transaction1 = Transaction.new({
+  "description" => "Weekly shop",
+  "value" => 72.59,
+  "date" => "14/1/2018"
+  "merchant_id" => merchant1.id,
+  "tag_id" => tag1.id
+})
+transaction1.save()
+transaction2 = Transaction.new({
+  "description" => "Pizza for poker game",
+  "value" => 23.99,
+  "date" => "23/1/2018"
+  "merchant_id" => merchant2.id,
+  "tag_id" => tag2.id
+})
+transaction2.save()
+transaction3 = Transaction.new({
+  "description" => "Beers while in town",
+  "value" => 10,
+  "date" => "24/1/2018"
+  "merchant_id" => merchant3.id,
+  "tag_id" => tag4.id
+})
+transaction3.save()
+transaction4 = Transaction.new({
+  "description" => "My round at the social night",
+  "value" => 28.45,
+  "date" => "3/2/2018"
+  "merchant_id" => merchant4.id,
+  "tag_id" => tag4.id
+})
+transaction4.save()
+
+binding.pry
+nil
