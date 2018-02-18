a3a721faa08b6e597be58880d55ab99c3f9f6453
Ed Ostler <edward.o@simul8.com>
2018-02-18 15:43:03 +0000

    Adds an index view for the dashboard that displays total spend and filter by tag form. It also adds an index_tag view for displaying the sumbission of the filter form

--- app.rb
+++ app.rb
@@ -1,10 +1,23 @@
 require("sinatra")
 require("sinatra/contrib/all")
-require_relative("controllers/dashboard_controller")
-require_relative("controllers/merchants_controller")
-require_relative("controllers/tags_controller")
-require_relative("controllers/transactions_controller")
+# require_relative("controllers/dashboard_controller")
+# require_relative("controllers/merchants_controller")
+# require_relative("controllers/tags_controller")
+# require_relative("controllers/transactions_controller")
+require_relative("models/merchant.rb")
+require_relative("models/tag.rb")
+require_relative("models/transaction.rb")
 
-get '/banked' do
+get "/banked" do
+  @sum_total = Transaction.sum_values()
+  @tags = Tag.all()
   erb(:"dashboard/index")
+end
+
+post "/banked" do
+  @sum_total = Transaction.sum_values()
+  @tags = Tag.all()
+  @tag = Tag.find(params[:id])
+  @sum_tag_total = Tag.sum_tag_values(params[:id])
+  erb(:"dashboard/index_tag")
 end
